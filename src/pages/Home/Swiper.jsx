32d51e86193c3fac1a3c4c66c668cb9775e8abb3
Louis Mony <73167166+LouisMony@users.noreply.github.com>
2024-02-03 22:36:10 +0100

    end refont slider

--- src/pages/Home/Swiper.jsx
+++ src/pages/Home/Swiper.jsx
@@ -1,30 +1,51 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react';
 import { Swiper, SwiperSlide, useSwiper } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/pagination';
-import { Mousewheel, Pagination } from 'swiper/modules';
+import { Pagination } from 'swiper/modules';
 
 const SwiperElement = (props) => {
-  return (
-    <Swiper
-        direction={'vertical'}
-        slidesPerView={3}
-        spaceBetween={30}
-        pagination={{
-        clickable: true,
-        }}
-        modules={[Pagination]}
-        className="Home2__swiper"
-        rewind={true}
-        onSlideChange={(swiper) => props.handleChangeSlide(swiper)}
-    >
-        {props.sliderItems && props.sliderItems.map((item, index) => (
-            <SwiperSlide key={index} className="swiperItem">
-            <img src={item.background} alt="Vignette" />
-            </SwiperSlide>
-        ))}
-    </Swiper>
-  )
-}
+    
+    // useEffect(() => {
+    //     if (props.sliderItems) {
+    //         document.addEventListener('click', handleClick);
+    //         return () => {
+    //             document.removeEventListener('click', handleClick);
+    //         };
+    //     }
+    // }, [props.sliderItems]);
 
-export default SwiperElement
+    const handleClick = async (event, swiper) => {
+        const mouseY = event.clientY;
+        const windowHeight = window.innerHeight;
+        const isUpperHalf = mouseY < windowHeight / 2;
+        if (isUpperHalf) swiperRef.current.slidePrev();
+        else swiperRef.current.slideNext();
+    };
+
+    const swiperRef = useRef();
+
+    return (
+        <Swiper
+            direction={'vertical'}
+            slidesPerView={1}
+            spaceBetween={30}
+            modules={[Pagination]}
+            className="Home2__swiper"
+            rewind={true}
+            speed={1000}
+            onSlideChange={(swiper) => props.handleChangeSlide(swiper)}
+            onSwiper={(swiper) => {swiperRef.current = swiper}}
+            onClick={(swiper) => handleClick(swiper)}
+        >
+            {props.sliderItems &&
+                props.sliderItems.map((item, index) => (
+                    <SwiperSlide key={index} className="swiperItem">
+                        <img src={item.background} alt="Vignette" />
+                    </SwiperSlide>
+                ))}
+        </Swiper>
+    );
+};
+
+export default SwiperElement;
